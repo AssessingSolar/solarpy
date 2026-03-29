ab63044ed13f1a09b54476860d74e5dcb6b500ac
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2026-03-29 22:47:49 +0200

    Add resample_to_freq function

--- src/solarpy/__init__.py
+++ src/solarpy/__init__.py
@@ -12,4 +12,5 @@ from solarpy import (  # noqa: F401
     horizon,
     example,
     iotools,
+    processing,
 )
---
+++ src/solarpy/processing/resample.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def resample_to_freq(data, freq, full_days=True, verbose=True):
+    start, end = data.index.min(), data.index.max()
+    if full_days:  # ensure all timesteps in the start and end day are present
+        start = start.replace(hour=0, minute=0, second=0, microsecond=0)
+        end = end.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+    full_index = pd.date_range(start, end, freq=freq)
+
+    if verbose:
+        n_total = len(full_index)
+        n_added = len(full_index.difference(data.index))
+        n_discarded = len(data.index.difference(full_index))
+        print(f"Resampled: {n_added} timesteps added ({n_added/n_total*100:2.1f}%), {n_discarded} discarded ({n_discarded/n_added*100:2.1f}%)")
+
+    data = data.reindex(full_index)
+
+    return data
