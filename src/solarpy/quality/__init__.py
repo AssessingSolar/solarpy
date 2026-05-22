eff597f820d46f78b23fe5d2e69e5f1b43d2173a
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2026-05-22 17:11:50 +0200

    Update BSRN closure/consistency quality flag function (#60)
    * Update comparison.py
    
    * Add function to documentation/package and tests

--- src/solarpy/quality/__init__.py
+++ src/solarpy/quality/__init__.py
@@ -1,3 +1,4 @@
 from solarpy.quality.limits import bsrn_limits  # noqa: F401
 from solarpy.quality.limits import bsrn_limits_flag  # noqa: F401
 from solarpy.quality.comparison import diffuse_fraction_flag  # noqa: F401
+from solarpy.quality.comparison import closure_flag  # noqa: F401
