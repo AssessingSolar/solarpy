2f82c121d8dd49cb828e9117405a4bacb2591875
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2026-06-14 21:43:54 +0200

    Add function to package and documentation

--- src/solarpy/quality/__init__.py
+++ src/solarpy/quality/__init__.py
@@ -2,3 +2,4 @@ from solarpy.quality.limits import bsrn_limits  # noqa: F401
 from solarpy.quality.limits import bsrn_limits_flag  # noqa: F401
 from solarpy.quality.comparison import diffuse_fraction_flag  # noqa: F401
 from solarpy.quality.comparison import closure_flag  # noqa: F401
+from solarpy.quality.t16_flags import t16_quality_flags  # noqa: F401
