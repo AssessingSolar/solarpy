bed7b8764155ead82adb1f890bcaa27fc91bcd88
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2026-07-02 17:14:51 -0800

    Lint format

--- docs/notebooks/horizon.py
+++ docs/notebooks/horizon.py
@@ -21,18 +21,28 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    latitude = mo.ui.number(start=-90, stop=90, value=55.79, step=0.01, label="Latitude [°N]")
-    longitude = mo.ui.number(start=-180, stop=180, value=12.53, step=0.01, label="Longitude [°E]")
-    altitude = mo.ui.number(start=-10, stop=8000, value=0, step=0.1, label="Altitude [m]")
-    ground_offset = mo.ui.number(start=-100, stop=200, value=0, step=0.1, label="Ground offset [m]")
+    latitude = mo.ui.number(
+        start=-90, stop=90, value=55.79, step=0.01, label="Latitude [°N]"
+    )
+    longitude = mo.ui.number(
+        start=-180, stop=180, value=12.53, step=0.01, label="Longitude [°E]"
+    )
+    altitude = mo.ui.number(
+        start=-10, stop=8000, value=0, step=0.1, label="Altitude [m]"
+    )
+    ground_offset = mo.ui.number(
+        start=-100, stop=200, value=0, step=0.1, label="Ground offset [m]"
+    )
 
-    mo.vstack([
-        mo.md("Select location of interest"),
-        latitude,
-        longitude,
-        altitude,
-        ground_offset,
-    ])
+    mo.vstack(
+        [
+            mo.md("Select location of interest"),
+            latitude,
+            longitude,
+            altitude,
+            ground_offset,
+        ]
+    )
     return altitude, ground_offset, latitude, longitude
 
 
@@ -65,9 +75,9 @@ def _(horizon, plt):
     ax.set_xticks(range(0, 361, 90))
     # ax.set_yticklabels([f"{t:2.1f}°" for t in ax.get_yticks()])
     ax.set_xticklabels(
-        ["N\n0°", "E\n90°", "S\n180°", "W\n270°", "N\n360°"],
-        ha='center')
-    ax.axhline(0, c='k', lw=3, zorder=-2)
+        ["N\n0°", "E\n90°", "S\n180°", "W\n270°", "N\n360°"], ha="center"
+    )
+    ax.axhline(0, c="k", lw=3, zorder=-2)
     ax.grid(True, alpha=0.3)
     plt.tight_layout()
     fig
