315e142c9359fd94d1b5f10f6c8c3ffafca7f304
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2026-09-21 14:35:08 +0200

    Update default limit to 0.5
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>

--- src/solarpy/plotting/shading_heatmap.py
+++ src/solarpy/plotting/shading_heatmap.py
@@ -15,7 +15,7 @@ def plot_shading_heatmap(
     value: Any,
     solar_azimuth: Any,
     solar_elevation: Any,
-    min_solar_elevation: float = 0.2,
+    min_solar_elevation: float = 0.5,
     azimuth_bin_size: float = 1.0,
     elevation_bin_size: float = 1.0,
     encoding: Callable[[np.ndarray], float] | str = "max",
@@ -50,7 +50,7 @@ def plot_shading_heatmap(
         be the same length as *value*.
     min_solar_elevation : array-like of float
         Minimum solar elevation angle in degrees for which to use data
-        for. Default is ``0.2``.
+        for. Default is ``0.5``.
     azimuth_bin_size : float, optional
         Width of each azimuth bin in degrees. Default is ``1.0``.
     elevation_bin_size : float, optional
